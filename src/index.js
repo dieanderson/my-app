70795168102b3660bb17d078d43e513e17a5e854
Diego Anderson <dieanderson@gmail.com>
2022-02-07 15:05:33 -0300

    adicionando template default

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+
 import App from './App'
+import './index.css'
+import { red, deepPurple } from '@mui/material/colors'
+
+const theme =  createTheme({
+  palette: {
+    primary:{
+      main: red[900],
+    },
+    secondary:{
+      main: deepPurple[900]
+    } 
+  },
+})
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>   
   </React.StrictMode>,
   document.getElementById('root')
 )
